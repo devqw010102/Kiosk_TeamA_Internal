6d7ae9aa00b91b43bc1dcfafab2a0ae419472505
KimByungKook <qw010102@gmail.com>
2026-05-14 22:57:52 +0900

    Feat: KioskInternal 초기 설정

---
+++ src/main/java/kr/hcnc/web/StudentApiController.java
@@ -0,0 +1,23 @@
+package kr.hcnc.web;
+
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import kr.hcnc.service.BadgeService;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentApiController {
+
+    @Resource(name = "badgeService")
+    private BadgeService badgeService;
+
+    @GetMapping("/search")
+    public List<Map<String, Object>> searchStudent(String param) {
+        System.out.println("StudentApiController :: /api/student/search");
+        return badgeService.selectStudents(param);
+    }
+}
