f4291afcbb5f537761330eb59abce50da29f35c7
KimByungKook <qw010102@gmail.com>
2026-05-23 17:15:20 +0900

    Feat: 전화번호 컬럼 추가

--- src/main/java/kr/hcnc/vo/StudentSearchVO.java
+++ src/main/java/kr/hcnc/vo/StudentSearchVO.java
@@ -4,6 +4,7 @@ public class StudentSearchVO {
     private String studentId;
     private String birthDate;
     private String dormitoryId;
+    private String phoneNumber;
 
     public String getStudentId() { return studentId; }
     public void setStudentId(String studentId) { this.studentId = studentId; }
@@ -12,5 +13,8 @@ public class StudentSearchVO {
     public void setBirthDate(String birthDate) { this.birthDate = birthDate; }
 	
     public String getDormitoryId() { return dormitoryId; }
-	public void setDormitoryId(String dormitoryId) { this.dormitoryId = dormitoryId; }
+    public void setDormitoryId(String dormitoryId) { this.dormitoryId = dormitoryId; }
+    
+	public String getPhoneNumber() {return phoneNumber;}
+	public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 }
