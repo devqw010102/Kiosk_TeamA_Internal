b69cec037c30dbdfa1a164f4ef15a1983a2e1f97
Jaydevlee <yousick5@gmail.com>
2026-06-01 09:44:54 +0900

    Modify: Put에서 patch로 수정 및 쿼리 조건 추

--- src/main/java/kr/hcnc/web/admin/AdminAttendanceController.java
+++ src/main/java/kr/hcnc/web/admin/AdminAttendanceController.java
@@ -1,8 +1,6 @@
 package kr.hcnc.web.admin;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -10,12 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.hcnc.service.admin.AdminAttendanceService;
@@ -47,7 +43,7 @@ public class AdminAttendanceController {
     }
     
     // 출석 상태 변경 및 사유 기재
-    @PutMapping("/{studentId}")
+    @PatchMapping("/{studentId}")
     public ResponseEntity<Integer> updateAttendMsg(@PathVariable String studentId, @RequestBody AttendVO attendVO) {
     	log.info("Called::updateAttendMsg/{}", studentId);
     	attendVO.setStudentId(studentId);
