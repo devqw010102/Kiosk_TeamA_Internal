713e3485a8a8ec72e03cde8936fffdaedfeadf96
daeya0406 <daeya0406@gmail.com>
2026-06-10 15:02:48 +0900

    feat: 교통 시간표 추가(#114)

--- src/main/java/kr/hcnc/vo/TransportVO.java
+++ src/main/java/kr/hcnc/vo/TransportVO.java
@@ -3,7 +3,10 @@ package kr.hcnc.vo;
 public class TransportVO {
 	private String transportId;
 	private String type;
-	private String content;
+	private String departLocation;
+	private String destination;
+	private String departTime;
+	private String arriveTime;
 	private String createdAt;
 	private String updatedAt;
 	private String delYn;
@@ -14,8 +17,17 @@ public class TransportVO {
 	public String getType() { return type; }
 	public void setType(String type) { this.type = type; }
 
-	public String getContent() { return content; }
-	public void setContent(String content) { this.content = content; }
+	public String getDepartLocation() { return departLocation; }
+	public void setDepartLocation(String departLocation) { this.departLocation = departLocation; }
+
+	public String getDestination() { return destination; }
+	public void setDestination(String destination) { this.destination = destination; }
+
+	public String getDepartTime() { return departTime; }
+	public void setDepartTime(String departTime) { this.departTime = departTime; }
+
+	public String getArriveTime() { return arriveTime; }
+	public void setArriveTime(String arriveTime) { this.arriveTime = arriveTime; }
 
 	public String getCreatedAt() { return createdAt; }
 	public void setCreatedAt(String createdAt) { this.createdAt = createdAt; }
